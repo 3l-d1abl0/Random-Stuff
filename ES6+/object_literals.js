d604488ee937bfdad240100924cbc71f41a6232d
Sameer <sameer.barha12@gmail.com>
2018-07-23 08:26:13 +0100

    object literal

---
+++ ES6+/object_literals.js
@@ -0,0 +1,9 @@
+//If the Key and value is same , you need not specifiy the key
+function addressMaker(city, state){
+
+  const newAddress = {city: city, state: state};
+
+  console.log(newAddress);
+}
+
+addressMaker('The Throne','Asgard');
