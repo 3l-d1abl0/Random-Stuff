79f3ad84fda690321917b32976493c0a331ea239
Sameer <sameer.barha12@gmail.com>
2018-07-23 10:11:39 +0100

    Includes

---
+++ ES6+/includes.js
@@ -0,0 +1,9 @@
+ //find out if any string contains a particular character or a substring
+//if any array contains an element
+
+let arr = [1,2,3,4,5];
+
+console.log(arr.includes(33));
+
+let str = 'Hello World';
+console.log(str.includes('Hello')); // true
